e0c2a1a238282c2bb850c1bffec964f2f031c7c8
Fantino-Russo <fantinojavierrusso@gmail.com>
2023-10-22 22:35:44 -0300

    agregando codigo de movimiento al jugador

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,12 @@
 const jugador = document.getElementById("jugador");
+const limite = document.querySelector(".pared");
+let posicion = 350;
+document.addEventListener('keydown', function(event){
+    if (event.key === 'ArrowLeft'){
+        posicion-=10;
+    }
+    else if (event.key === 'ArrowRight'){
+        posicion+=10;
+    }
+    jugador.style.left=`${posicion}px`;
+})
