04cda9c0f22543b44f0ebaaf3bda9aef0fa84ad5
Fantino-Russo <fantinojavierrusso@gmail.com>
2023-10-22 23:25:27 -0300

    retocando velocidad de auto

--- JS/script.js
+++ JS/script.js
@@ -3,10 +3,11 @@ const limite = document.querySelector(".pared");
 let posicion = 350;
 document.addEventListener('keydown', function(event){
     if (event.key === 'ArrowLeft'){
-        posicion-=10;
+        posicion-=5;
     }
     else if (event.key === 'ArrowRight'){
-        posicion+=10;
+        posicion+=5;
     }
+    
     jugador.style.left=`${posicion}px`;
 })
