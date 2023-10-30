3a7b02701aab754cba6ca8bf7bdcd2da3e648aca
Fantino-Russo <fantinojavierrusso@gmail.com>
2023-10-30 14:51:56 -0300

    Agregando los limites de la calle

--- JS/script.js
+++ JS/script.js
@@ -1,13 +1,14 @@
 const jugador = document.getElementById("jugador");
 const limite = document.querySelector(".pared");
-let posicion = 350;
+posicion = 350;
+velocidad = 1;
 document.addEventListener('keydown', function(event){
-    if (event.key === 'ArrowLeft'){
-        posicion-=5;
+    if ((event.key === 'ArrowLeft') && (posicion>210) ){
+        posicion -= 5 * velocidad;
     }
-    else if (event.key === 'ArrowRight'){
-        posicion+=5;
+    else if ((event.key === 'ArrowRight') && (posicion<490)){
+        posicion += 5 * velocidad;
     }
     
-    jugador.style.left=`${posicion}px`;
-})
+    jugador.style.left = `${posicion}px`;
+});
