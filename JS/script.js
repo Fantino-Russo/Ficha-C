56129f210c167173075b3f4115c23d15829b4a17
Fantino.Russo <fantinojavierrusso@gmail.com>
2023-11-08 20:19:25 -0300

    Agregando generador random de autos (no completo)

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,8 @@
 const jugador = document.getElementById("jugador");
+const tablero = document.getElementById("tablero")
 jugador.posicion = 350;
 jugador.velocidad = 1;
+const Tipos = ["AutoDerecha1", "AutoDerecha2", "AutoCentro1", "AutoCentro2", "AutoIzquierda1", "AutoIzquierda2"];
 document.addEventListener('keydown', function(event){
     if ((event.key === 'ArrowLeft') && (jugador.posicion>210) ){
         jugador.posicion -= 5 * jugador.velocidad;
@@ -11,3 +13,16 @@ document.addEventListener('keydown', function(event){
     
     jugador.style.left = `${jugador.posicion}px`;
 });
+function crearAutos(){
+    const autoEnemigo = document.createElement('div').className("Autoenemigo1");
+
+    // autoEnemigo.className = Tipos[Math.floor(Math.random() * Tipos.length)];
+    tablero.appendChild(autoEnemigo);
+    autoEnemigo.style.top = Math.random()*100;
+}
+// let Generador = Math.floor(Math.random() * 10);
+// console.log(Generador);
+
+crearAutos();
+
+
