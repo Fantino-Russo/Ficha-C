820d930fc564071210f02bb5658436160d1e7062
Fantino-Russo <fantinojavierrusso@gmail.com>
2023-11-11 11:09:17 -0300

    Agregando esqueletos de funciones y eliminando el generador de autos

--- JS/script.js
+++ JS/script.js
@@ -2,7 +2,6 @@ const jugador = document.getElementById("jugador");
 const tablero = document.getElementById("tablero")
 jugador.posicion = 350;
 jugador.velocidad = 1;
-const Tipos = ["AutoDerecha1", "AutoDerecha2", "AutoCentro1", "AutoCentro2", "AutoIzquierda1", "AutoIzquierda2"];
 document.addEventListener('keydown', function(event){
     if ((event.key === 'ArrowLeft') && (jugador.posicion>210) ){
         jugador.posicion -= 5 * jugador.velocidad;
@@ -13,16 +12,9 @@ document.addEventListener('keydown', function(event){
     
     jugador.style.left = `${jugador.posicion}px`;
 });
-function crearAutos(){
-    const autoEnemigo = document.createElement('div').className("Autoenemigo1");
 
-    // autoEnemigo.className = Tipos[Math.floor(Math.random() * Tipos.length)];
-    tablero.appendChild(autoEnemigo);
-    autoEnemigo.style.top = Math.random()*100;
+function verificarColision(){
+    
 }
-// let Generador = Math.floor(Math.random() * 10);
-// console.log(Generador);
-
-crearAutos();
 
 
